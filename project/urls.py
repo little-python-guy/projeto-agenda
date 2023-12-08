deabb00683d1b89d65887def4d94189a46cd893e
little-python-guy <matat201@gmail.com>
2023-12-08 16:43:51 -0300

    Adicionando path('admin/') anteriormente apagado

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('contact.urls')),
+    path('', include('contact.urls'),),
+    path('admin/', admin.site.urls),
 ]
