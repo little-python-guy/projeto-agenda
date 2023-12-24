1fe82fc50a9cc5e98fb72a4142c95f1ed358ca1c
little-python-guy <matat201@gmail.com>
2023-12-24 10:31:51 -0300

    Habilitando uma busca simples entre os campos da página

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
+from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
@@ -10,11 +11,40 @@ def index(request):
         .filter(show=True)\
         .order_by('-id')
 
-    print(contacts.query)
 
     context = {
         'contacts': contacts,
         'site_title': "Contatos - ",
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+def search(request):
+
+    search_value = request.GET.get('q', ' ').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) | 
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    print(contacts.query)
+
+    context = {
+        'contacts': contacts,
+        'site_title': "Busca - ",
     }
 
     return render(
