a604602646bd2f27eec2fbb07725264b79264415
little-python-guy <matat201@gmail.com>
2024-01-02 18:24:00 -0300

    Configurando os campos e widgets do formulário

--- contact/forms.py
+++ contact/forms.py
@@ -5,6 +5,26 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para o seu usuário',
+
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Escreva aqui',
+        # })
+
     class Meta:
         model = models.Contact
         fields = (
@@ -12,6 +32,15 @@ class ContactForm(forms.ModelForm):
             'last_name',
             'phone'
         ) 
+
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     ),
+        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
