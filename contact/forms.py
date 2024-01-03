877a922b94c9291b43c53ebfdb795cf927c73304
little-python-guy <matat201@gmail.com>
2024-01-03 14:34:53 -0300

    Adicionando validação de dados dos campos 'first_name' e 'last_name'

--- contact/forms.py
+++ contact/forms.py
@@ -45,20 +45,37 @@ class ContactForm(forms.ModelForm):
     def clean(self):
         cleaned_data = self.cleaned_data
         
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
+        if first_name == last_name:
+
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid',
             )
-        )
+
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
 
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        # raise ValidationError(
+        #     'Veio do raise',
+        #     code='invalid',
+        # )
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add error',
+                    code='invalid'
+                )
+            )
+
+        return first_name
