f3b9dbe0aaeddca4702ee93e4cf35c90ab15bf31
little-python-guy <matat201@gmail.com>
2024-01-05 16:16:14 -0300

    Acrescentando 'request.FILES'

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -14,7 +14,7 @@ def create(request):
     if request.method == 'POST':
 
 
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
 
         context = {
             'form': form,
@@ -52,7 +52,7 @@ def update(request, contact_id):
     if request.method == 'POST':
 
 
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form,
