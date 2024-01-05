582413f91ced4c94dc65c6a5e7f4af2ab98bc018
little-python-guy <matat201@gmail.com>
2024-01-05 16:18:14 -0300

    Adicionando campo para inserir imagem no contato

--- contact/forms.py
+++ contact/forms.py
@@ -5,25 +5,14 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'Aqui veio do init',
+                'accept': 'image/*',
             }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para o seu usuário',
-
+        )
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Escreva aqui',
-        # })
 
     class Meta:
         model = models.Contact
@@ -34,16 +23,8 @@ class ContactForm(forms.ModelForm):
             'email',
             'description',
             'category',
+            'picture',
         ) 
-
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     ),
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
